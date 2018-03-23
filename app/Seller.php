e44926b2da00ac6ef53dbcee8960d21fb7007e12
Victor Vivenzio <victorvivenzio@gmail.com>
2018-03-23 17:38:59 +0100

    Instance Inyection and Global Scope

--- app/Seller.php
+++ app/Seller.php
@@ -4,9 +4,17 @@ namespace App;
 
 use App\User;
 use App\Product;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+	protected static function boot()
+	{
+		parent::boot();
+
+		static::addGlobalScope( new SellerScope );
+	}
+
     public function products()
     {
     	return $this->hasMany( Product::class );
